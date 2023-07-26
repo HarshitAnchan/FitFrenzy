31250c1f4addb8b313862019fe969eb0012ac618
harshitanchan <harshitanchan647@gmail.com>
2023-07-26 21:06:23 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,15 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '5d5e7afb5emshb7cf4e9c823c002p12c282jsn88d4345f0cd4',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    }
+  };
+
+
+export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
