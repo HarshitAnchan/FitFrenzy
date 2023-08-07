13b2f09c5a99c26a79e01723d205827fe9d6b546
Harshit Anchan <88927163+HarshitAnchan@users.noreply.github.com>
2023-08-07 21:39:46 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -13,7 +13,7 @@ export const youtubeOptions = {
   method: 'GET',
   
   headers: {
-    'X-RapidAPI-Key': '5d5e7afb5emshb7cf4e9c823c002p12c282jsn88d4345f0cd4',
+    'X-RapidAPI-Key': 'Your Api',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
   
